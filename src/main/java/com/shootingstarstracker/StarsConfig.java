8929b387d8e002ae583aa30d7529f4a4029305c4
Cody Lyttle <codylyttle123@gmail.com>
2023-10-18 00:52:47 +1300

    Remove unused config item.

--- src/main/java/com/shootingstarstracker/StarsConfig.java
+++ src/main/java/com/shootingstarstracker/StarsConfig.java
@@ -2,18 +2,8 @@ package com.shootingstarstracker;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
-import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("stars")
 public interface StarsConfig extends Config
 {
-	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
-	)
-	default String greeting()
-	{
-		return "Hello";
-	}
 }
